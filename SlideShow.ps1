e06cff5272e489b710e2b4981bdd26b5dda5ec2a
Michael Aylmer <michael.aylmer@coastautonomous.com>
2024-11-19 12:53:02 -0500

    Update fixed $env:USERNAME SlideShow.ps1

--- SlideShow.ps1
+++ SlideShow.ps1
@@ -1,3 +1,16 @@
-mkdir -Force "C:\Users\$env:USERNAME\Pictures\SlideShow"
-cd "C:\Users\$env:USERNAME\Pictures\SlideShow"
+# mkdir -Force "C:\Users\$env:USERNAME\Pictures\SlideShow"
+# cd "C:\Users\$env:USERNAME\Pictures\SlideShow"
+# & "C:\Program Files\Git\cmd\git.exe" clone https://github.com/CoastAylmer/Slide-Show-Images.git .
+
+# Dynamically retrieve the logged-in user's name
+$userName = (Get-WmiObject -Class Win32_ComputerSystem).UserName.Split('\\')[1]
+
+# Create the directory if it doesn't exist
+$slideShowPath = "C:\Users\$userName\Pictures\SlideShow"
+mkdir -Force $slideShowPath
+
+# Change to the directory
+cd $slideShowPath
+
+# Clone the Git repository using the correct path to git.exe
 & "C:\Program Files\Git\cmd\git.exe" clone https://github.com/CoastAylmer/Slide-Show-Images.git .
