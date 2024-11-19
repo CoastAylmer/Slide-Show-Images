c21ff6cf0f1d8648bcdd69d0cb18eb002b1c9a32
Michael Aylmer <michael.aylmer@coastautonomous.com>
2024-11-19 10:23:36 -0500

    Update mkdir command

--- SlideShow.ps1
+++ SlideShow.ps1
@@ -1,3 +1,4 @@
-mkdir -Force "C:\Users\$env:USERNAME\Pictures\SlideShow"
-cd "C:\Users\$env:USERNAME\Pictures\SlideShow"
+mkdir -Force ".\SlideShow"
+cd ".\SlideShow"
 & "C:\Program Files\Git\cmd\git.exe" clone https://github.com/CoastAylmer/Slide-Show-Images.git .
+
