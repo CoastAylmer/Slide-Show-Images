1ec2a5c55aa52d8fb44e965b0702d68c6d1cf9c0
Michael Aylmer <michael.aylmer@coastautonomous.com>
2024-11-19 11:10:26 -0500

    Revert back to previous version of SlideShow.ps1

--- SlideShow.ps1
+++ SlideShow.ps1
@@ -1,12 +1,3 @@
-
-Write-Output "Current user: $env:USERNAME"
-
-# Create the directory
 mkdir -Force "C:\Users\$env:USERNAME\Pictures\SlideShow"
-
-# Change to the correct directory
 cd "C:\Users\$env:USERNAME\Pictures\SlideShow"
-
-# Clone the repository using the full path to git.exe
 & "C:\Program Files\Git\cmd\git.exe" clone https://github.com/CoastAylmer/Slide-Show-Images.git .
-
