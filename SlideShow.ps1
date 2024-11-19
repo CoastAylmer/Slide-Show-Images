173f8c238620469a086c005d448ba7f121e41c6a
Michael Aylmer <michael.aylmer@coastautonomous.com>
2024-11-19 10:15:40 -0500

    Update SlideShow.ps1 to use git PATH

--- SlideShow.ps1
+++ SlideShow.ps1
@@ -1,3 +1,3 @@
 mkdir -Force "C:\Users\$env:USERNAME\Pictures\SlideShow"
 cd "C:\Users\$env:USERNAME\Pictures\SlideShow"
-git clone https://github.com/CoastAylmer/Slide-Show-Images.git .
+& "C:\Program Files\Git\cmd\git.exe" clone https://github.com/CoastAylmer/Slide-Show-Images.git .
